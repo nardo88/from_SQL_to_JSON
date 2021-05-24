78d09508ac50d18176144193fed30d1125b83dc6
nardo88 <nardo1988@mail.ru>
2021-05-24 21:11:24 +0300

    1

--- content.php
+++ content.php
@@ -1,36 +1,32 @@
 <?php
-
-$connection = new PDO ('mysql:host=localhost; dbname=magazine; charset=utf8', 'root', 'root');
-
-$content = $connection->query("SELECT * FROM content");
-
-
-$items = [];
-
-class Item {
-    public function __construct($id, $name, $description, $price, $image) {
-        $this->id = $id;
-        $this->name = $name;
-        $this->description = $description;
-        $this->price = $price;
-        $this->image = $image;
+// устанавливаем соединение
+    $connection = new PDO ('mysql:host=localhost; dbname=magazine; charset=utf8', 'root', 'root');
+    // получаем данные из таблицы
+    $content = $connection->query("SELECT * FROM content");
+    // массив который будем переводить в JSON
+    $items = [];
+    // класс на основании которого будем создавать объекты дл ямассива
+    class Item {
+        public function __construct($id, $name, $description, $price, $image) {
+            $this->id = $id;
+            $this->name = $name;
+            $this->description = $description;
+            $this->price = $price;
+            $this->image = $image;
+        }
     }
-}
-
-
-
-foreach($content as $item){
-    $newItem = new Item($item['id'], $item['name'], $item['description'], $item['price'], $item['image'], );
-    array_push($items, $newItem);
-}
-
-
-$items = json_encode($items);
-header('Content-type: application/json');
-
-file_put_contents('test.json', $items);
-
-echo $items;
-
+    // заполняем массив объектами
+    foreach($content as $item){
+        $newItem = new Item($item['id'], $item['name'], $item['description'], $item['price'], $item['image'], );
+        array_push($items, $newItem);
+    }
+    // кодируем в JSON
+    $items = json_encode($items);
+    // на всякий случай прописываем заголовки
+    header('Content-type: application/json');
+    // для теста записываем в файл JSON (просто так)
+    file_put_contents('test.json', $items);
+    // возвращаем JSON
+    echo $items;
 ?>
 
